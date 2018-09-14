73732ac81e208052c022c23eba0652e7896dc55e
Nicholas Dille <nicholas.dille@mailbox.org>
2018-09-14 13:12:21 +0200

    Fixed installation path

--- master.sh
+++ master.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Instructions taken from https://kubernetes.io/docs/setup/independent/install-kubeadm/
+# Changed installation directory for tools
 
 # Install Docker
 apt-get update
@@ -17,23 +18,21 @@ curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_
 
 # Install CRI tools
 CRICTL_VERSION="v1.11.1"
-mkdir -p /opt/bin
-curl -L "https://github.com/kubernetes-incubator/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C /opt/bin -xz
+curl -L "https://github.com/kubernetes-incubator/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C /usr/local/bin -xz
 
 # Install kubernetes tools
 RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
-mkdir -p /opt/bin
-cd /opt/bin
+cd /usr/local/bin
 curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
 chmod +x {kubeadm,kubelet,kubectl}
-curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/kubelet.service" | sed "s:/usr/bin:/opt/bin:g" > /etc/systemd/system/kubelet.service
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/kubelet.service" | sed "s:/usr/bin:/usr/local/bin:g" > /etc/systemd/system/kubelet.service
 mkdir -p /etc/systemd/system/kubelet.service.d
-curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/10-kubeadm.conf" | sed "s:/usr/bin:/opt/bin:g" > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/10-kubeadm.conf" | sed "s:/usr/bin:/usr/local/bin:g" > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl enable kubelet
 systemctl start kubelet
 
 # Init cluster
-/opt/bin/kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=10.244.0.0/16
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=10.244.0.0/16
 sed -i 's/- kube-apiserver/- kube-apiserver\n    - --service-node-port-range=1-65535/' /etc/kubernetes/manifests/kube-apiserver.yaml
 service kubelet restart
 
@@ -43,7 +42,7 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Init networking
-/opt/bin/kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # Enable scheduling on master
-/opt/bin/kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-
+kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-
