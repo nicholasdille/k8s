adf9979a2885742fddbc40e1c5c72009ad4f8147
Nicholas Dille <nicholas.dille@mailbox.org>
2018-09-23 11:05:05 +0200

    Separated prerequisites and cluster init

--- master.sh
+++ master.sh
@@ -4,13 +4,6 @@ set -e
 # Instructions taken from https://kubernetes.io/docs/setup/independent/install-kubeadm/
 # Changed installation directory for tools
 
-# Install Docker
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
-
 # Install CNI plugins
 CNI_VERSION="v0.6.0"
 mkdir -p /opt/cni/bin
