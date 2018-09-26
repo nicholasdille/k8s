76038317c53b7406255a98ba71d75f97ab9ee9c5
Nicholas Dille <nicholas.dille@mailbox.org>
2018-09-26 14:29:21 +0200

    Added docker-compose

--- docker.sh
+++ docker.sh
@@ -7,3 +7,7 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
 apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+
+# Install docker-compose
+curl -Ls https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
