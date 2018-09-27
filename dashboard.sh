4e081cfcc02e94b4bae07e24f4d00b513ca7425d
Nicholas Dille <nicholas.dille@mailbox.org>
2018-09-27 09:57:24 +0200

    Added dashboard deployment

--- dashboard.sh
+++ dashboard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+wget https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+patch kubernetes-dashboard.yml < dashboard.patch
 kubectl -n kube-systemys create serviceaccount my-dashboard-sa
 kubectl -n kube-system create clusterrolebinding my-dashboard-sa --clusterrole=cluster-admin --serviceaccount=kube-system:my-dashboard-sa
 kubectl apply -f dashboard.yml
