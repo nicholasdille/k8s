2887572da92450bc530b49880c6fad1d56ee087e
Nicholas Dille <nicholas.dille@mailbox.org>
2018-10-03 13:03:11 +0200

    Fixed patch

--- dashboard.sh
+++ dashboard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-patch kubernetes-dashboard.yml < dashboard.patch
-kubectl -n kube-systemys create serviceaccount my-dashboard-sa
+patch kubernetes-dashboard.yaml < dashboard.patch
+kubectl -n kube-system create serviceaccount my-dashboard-sa
 kubectl -n kube-system create clusterrolebinding my-dashboard-sa --clusterrole=cluster-admin --serviceaccount=kube-system:my-dashboard-sa
-kubectl apply -f dashboard.yml
+kubectl apply -f kubernetes-dashboard.yaml
+kubectl apply -f dashboard-ingress.yml
