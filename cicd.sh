e6c24ce434b8ebf1a97e5cc92e4dd149307fbdb4
Nicholas Dille <nichcolas.dille@mailbox.org>
2018-10-04 15:55:24 +0200

    Added DooD agent for drone

--- cicd.sh
+++ cicd.sh
@@ -22,8 +22,9 @@ cat drone.yml | envsubst | kubectl apply -f -
 curl -Ls https://github.com/drone/drone-cli/releases/download/v0.8.6/drone_linux_amd64.tar.gz | tar -xvz -C /usr/local/bin
 
 # Install drone agent
-kubectl apply -f dind.yml
-kubectl apply -f drone-agent.yml
+#kubectl apply -f dind.yml
+#kubectl apply -f drone-agent.yml
+kubectl apply -f drone-agent-dood.yml
 
 # Install WebDAV
 kubectl delete configmap webdav-auth
