5585a22a186890311ecf36f805d857886aa96fd0
Nicholas Dille <nicholas.dille@mailbox.org>
2018-10-11 12:03:29 +0200

    Updated Docker to 18.06 for k8s 1.12

--- docker.sh
+++ docker.sh
@@ -6,7 +6,13 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+cat > /etc/apt/preference.d/docker <<EOF
+Package: docker-ce
+Pin: version 18.06.*
+Pin-Priority: 1000
+EOF
+apt-get update
+apt-get install -y docker-ce
 
 # Install docker-compose
 curl -Ls https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
