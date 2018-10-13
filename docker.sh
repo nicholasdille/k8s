a97bdd872c98b6a86a02f38dc3b4a94f3b4f129d
Nicholas Dille <nicholas.dille@mailbox.org>
2018-10-13 10:17:13 +0200

    Fixed pinning of Docker

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-cat > /etc/apt/preference.d/docker <<EOF
+cat > /etc/apt/preferences.d/docker <<EOF
 Package: docker-ce
 Pin: version 18.06.*
 Pin-Priority: 1000
