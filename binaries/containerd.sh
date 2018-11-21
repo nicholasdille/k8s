c14d19caa540fcc71ad2a164d732d52e656bf24e
Nicholas Dille <nicholas.dille@mailbox.org>
2018-11-21 22:16:53 +0100

    Added default configuration

--- binaries/containerd.sh
+++ binaries/containerd.sh
@@ -5,6 +5,9 @@ CONTAINERD_VERSION=1.2.0
 
 curl -sL https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz | tar -xz -C /usr/local/
 
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+
 curl -sL https://github.com/containerd/containerd/raw/master/containerd.service > /etc/systemd/system/containerd.service
 
 apt-get update
@@ -14,3 +17,5 @@ mkdir -p /etc/systemd/system/kubelet.service.d/
 cp 0-containerd.conf /etc/systemd/system/kubelet.service.d/
 
 systemctl daemon-reload
+
+echo "runtime-endpoint: unix:///run/containerd/containerd.sock" > /etc/crictl.yaml
