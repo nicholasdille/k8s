b16bcd2846ab15fba8bb8e3a8a7bb697ae99be32
Nicholas Dille <nicholas.dille@mailbox.org>
2018-11-21 20:58:40 +0100

    Added CNI configuration

--- binaries/cni.sh
+++ binaries/cni.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+mkdir -p /opt/cni/bin /etc/cni/net.d
+
 CNI_VERSION="v0.6.0"
-mkdir -p /opt/cni/bin
-curl -sL "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+
+CNI_PLUGIN_VERSION="0.7.4"
+curl -L https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VERSION}/cni-plugins-amd64-v${CNI_PLUGIN_VERSION}.tgz | tar -C /opt/cni/bin -xz
+
+cp bridge.conf /etc/cni/net.d/
